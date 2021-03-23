35bce24a979e64d89c0182c88f21cd32c336602d
Karl Maier <karl.maier@link2brain.at>
2021-03-23 20:35:28 +0100

    remove unused files

--- imldiff/linearly_separable/README.md
+++
@@ -1,3 +0,0 @@
-# Linearly separable artificially generated dataset
-
-source: [scikit-learn 0.24, "Classifier comparison"](https://scikit-learn.org/0.24/auto_examples/classification/plot_classifier_comparison.html)
--- imldiff/linearly_separable/loader.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import make_classification
-import numpy as np
-
-
-def load_data(n_samples=100):
-    X, y = make_classification(n_samples=n_samples, n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1)
-    rng = np.random.RandomState(2)
-    X += 2 * rng.uniform(size=X.shape)
-    return X, y

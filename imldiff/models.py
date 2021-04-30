6a474df90444c398f730824e703045678f8e086c
Karl Maier <karl.maier@link2brain.at>
2021-04-30 17:04:54 +0200

    new notebook: logistic regression vs. stepped one

--- imldiff/models.py
+++ imldiff/models.py
@@ -1,5 +1,11 @@
 from scipy.special import logit, expit
 import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+class SteppedLogisticRegression(LogisticRegression):
+    def decision_function(self, X):
+        scores = super(SteppedLogisticRegression, self).decision_function(X)
+        return scores.astype(int).astype(float)
 
 
 class Classifier:

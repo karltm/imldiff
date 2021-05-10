35ca9e4e71472022cf81f943e2aa8dfeac1945f9
Karl Maier <karl.maier@link2brain.at>
2021-05-10 17:24:50 +0200

    update notebooks

--- imldiff/util.py
+++ imldiff/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import logsumexp
 import shap
     
 def reduce_multiclass_proba_diff_shap_values(shap_values):
@@ -16,6 +17,17 @@ def reduce_multiclass_proba_diff_shap_values(shap_values):
                             data=shap_values.data,
                             display_data=shap_values.display_data,
                             feature_names=shap_values.feature_names)
+
+def calc_binary_log_odds_from_log_proba(log_proba):
+    return log_proba[:, 1] - log_proba[:, 0]
+
+def calc_log_odds_from_log_proba(log_proba):
+    log_odds = np.empty(log_proba.shape)
+    for i in range(log_proba.shape[1]):
+        class_mask = [True] * log_proba.shape[1]
+        class_mask[i] = False
+        log_odds[:, i] = log_proba[:, i] - logsumexp(log_proba[:, class_mask], axis=1)
+    return log_odds
 
 def get_force_plot_ordering(plot):
     return list(map(lambda x: int(x['simIndex']), plot.data['explanations']))

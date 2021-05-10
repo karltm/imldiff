cd036a74e4d8db37d4abc565cf5b96a55e590e6d
Karl Maier <karl.maier@link2brain.at>
2021-05-10 17:22:58 +0200

    use scipy function instead of own implementation

--- imldiff/difference_models.py
+++ imldiff/difference_models.py
@@ -3,6 +3,7 @@ from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 import itertools
 import numpy as np
+from scipy.special import logsumexp
 
 
 def predict_log_proba(clf, X):
@@ -11,21 +12,9 @@ def predict_log_proba(clf, X):
     else:
         proba = clf.predict_proba(X)
         return np.log(proba)
-    
-def add_in_log_proba_space(log_proba1, log_proba2):
-    if np.isfinite(log_proba1) and np.isfinite(log_proba2):
-        return log_proba1 + np.log1p(np.exp(log_proba2 - log_proba1))
-    if np.isneginf(log_proba1) and np.isneginf(log_proba2):
-        return -np.inf
-    if np.isneginf(log_proba1) and np.isfinite(log_proba2):
-        return log_proba2
-    if np.isfinite(log_proba1) and np.isneginf(log_proba2):
-        return log_proba1
-    return np.nan
-    
-np_add_in_log_proba_space = np.frompyfunc(add_in_log_proba_space, 2, 1)
+
 complement_log_proba = lambda log_proba: np.log1p(-np.exp(log_proba))
-        
+
 
 class BinaryDifferenceClassifier(BaseEstimator, ClassifierMixin):
     """
@@ -72,7 +61,7 @@ class BinaryDifferenceClassifier(BaseEstimator, ClassifierMixin):
         X = check_array(X)
         log_proba_a = predict_log_proba(self.clf_a, X)
         log_proba_b = predict_log_proba(self.clf_b, X)
-        log_proba_pos = np_add_in_log_proba_space.reduce(log_proba_a + log_proba_b, axis=1).astype('float64')
+        log_proba_pos = logsumexp(log_proba_a + log_proba_b, axis=1)
         return np.vstack((complement_log_proba(log_proba_pos), log_proba_pos)).T
 
 

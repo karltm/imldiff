786e97eacca0dcef28529d6639ad192fcbce766e
Karl Maier <karl.maier@link2brain.at>
2021-05-10 18:16:29 +0200

    update notebooks with proposed methods

--- imldiff/util.py
+++ imldiff/util.py
@@ -18,6 +18,9 @@ def reduce_multiclass_proba_diff_shap_values(shap_values):
                             display_data=shap_values.display_data,
                             feature_names=shap_values.feature_names)
 
+def make_predict_one_hot_encoded_labels(n_classes, predict):
+    return lambda X: np.eye(n_classes)[predict(X)]
+
 def calc_binary_log_odds_from_log_proba(log_proba):
     return log_proba[:, 1] - log_proba[:, 0]
 

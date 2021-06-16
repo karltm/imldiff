0587f353233c967be35c71c47bf88e32559e517d
Karl Maier <karl.maier@link2brain.at>
2021-06-16 17:44:32 +0200

    update notebooks

--- util.py
+++ util.py
@@ -3,6 +3,10 @@ from scipy.special import logsumexp
 import shap
 from shap.utils import hclust_ordering
 from sklearn.decomposition import PCA
+from sklearn.metrics import brier_score_loss, precision_score, recall_score, f1_score
+from sklearn.calibration import CalibratedClassifierCV, calibration_curve
+from sklearn.linear_model import LogisticRegression
+from matplotlib import pyplot as plt
     
 def reduce_multiclass_proba_diff_shap_values(shap_values):
     """
@@ -51,6 +55,65 @@ def calc_log_odds_from_log_proba(log_proba):
     return log_odds
 
 
+def calibrate_classifier(est, name, X_train, X_test, y_train, y_test, cv=10, fig_index=1):
+    """Plot calibration curve for est w/o and with calibration. """
+    # Calibrated with isotonic calibration
+    isotonic = CalibratedClassifierCV(est, cv=cv, method='isotonic')
+
+    # Calibrated with sigmoid calibration
+    sigmoid = CalibratedClassifierCV(est, cv=cv, method='sigmoid')
+
+    # Logistic regression with no calibration as baseline
+    lr = LogisticRegression(C=1.)
+
+    fig = plt.figure(fig_index, figsize=(10, 10))
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 1), (2, 0))
+
+    ax1.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+    for clf, name in [(lr, 'Logistic (Baseline)'),
+                      (est, name),
+                      (isotonic, name + ' + Isotonic'),
+                      (sigmoid, name + ' + Sigmoid')]:
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        if hasattr(clf, "predict_proba"):
+            prob_pos = clf.predict_proba(X_test)[:, 1]
+        else:  # use decision function
+            prob_pos = clf.decision_function(X_test)
+            prob_pos = \
+                (prob_pos - prob_pos.min()) / (prob_pos.max() - prob_pos.min())
+
+        clf_score = brier_score_loss(y_test, prob_pos, pos_label=max(y_train.max(), y_test.max()))
+        print("%s:" % name)
+        print("\tBrier: %1.3f" % (clf_score))
+        print("\tPrecision: %1.3f" % precision_score(y_test, y_pred))
+        print("\tRecall: %1.3f" % recall_score(y_test, y_pred))
+        print("\tF1: %1.3f\n" % f1_score(y_test, y_pred))
+
+        fraction_of_positives, mean_predicted_value = \
+            calibration_curve(y_test, prob_pos, n_bins=10)
+
+        ax1.plot(mean_predicted_value, fraction_of_positives, "s-",
+                 label="%s (%1.3f)" % (name, clf_score))
+
+        ax2.hist(prob_pos, range=(0, 1), bins=10, label=name,
+                 histtype="step", lw=2)
+
+    ax1.set_ylabel("Fraction of positives")
+    ax1.set_ylim([-0.05, 1.05])
+    ax1.legend(loc="lower right")
+    ax1.set_title('Calibration plots  (reliability curve)')
+
+    ax2.set_xlabel("Mean predicted value")
+    ax2.set_ylabel("Count")
+    ax2.legend(loc="upper center", ncol=2)
+
+    plt.tight_layout()
+
+    return isotonic, sigmoid
+
+
 def calc_feature_order(shap_values):
     if len(shap_values.shape) == 2:
         values = np.abs(shap_values.values).mean(axis=0)
@@ -83,3 +146,5 @@ def calc_instance_order(shap_values):
     instance_order = np.argsort(hclust_ordering(values))
     return instance_order
     # informative_class_indices = class_order[:n_informative_classes]
+
+

77e1553ad75ac5bdb2c6744fc525cb9b600aa4ad
Karl Maier <maier.karl92@gmail.com>
2021-11-18 20:01:27 +0100

    replace own implementation of tree feature importances with sklearn's

--- imldiff/baseline.py
+++ imldiff/baseline.py
@@ -71,33 +71,3 @@ def get_rules(tree, feature_names, class_names, class_=None, feature_order=None,
             rule += f"class: {class_names[l]} ({int(classes[l])}/{int(sum(classes))} instances)"
         rules += [rule]
     return rules
-
-
-def dt_feature_importance(model, normalize=True):
-    """Source: https://stackoverflow.com/questions/49170296/scikit-learn-feature-importance-calculation-in-decision-trees"""
-
-    left_c = model.tree_.children_left
-    right_c = model.tree_.children_right
-
-    impurity = model.tree_.impurity
-    node_samples = model.tree_.weighted_n_node_samples
-
-    # Initialize the feature importance, those not used remain zero
-    feature_importance = np.zeros((model.tree_.n_features,))
-
-    for idx,node in enumerate(model.tree_.feature):
-        if node >= 0:
-            # Accumulate the feature importance over all the nodes where it's used
-            feature_importance[node]+=impurity[idx]*node_samples[idx]- \
-                                      impurity[left_c[idx]]*node_samples[left_c[idx]]- \
-                                      impurity[right_c[idx]]*node_samples[right_c[idx]]
-
-    # Number of samples at the root node
-    feature_importance/=node_samples[0]
-
-    if normalize:
-        normalizer = feature_importance.sum()
-        if normalizer > 0:
-            feature_importance/=normalizer
-
-    return feature_importance
